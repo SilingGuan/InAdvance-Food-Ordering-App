318ebe8147087eda58c0f3eb8e0aa75180bb318a
SilingGuan <47166788+SilingGuan@users.noreply.github.com>
2020-10-24 00:03:24 -0700

    delete codeactivity and move show needed data feature to QRcodeActivity

--- app/src/main/java/com/example/lab3map/MainActivity.java
+++ app/src/main/java/com/example/lab3map/MainActivity.java
@@ -1,6 +1,4 @@
 package com.example.lab3map;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
@@ -10,15 +8,15 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
